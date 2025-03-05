8db73b7fe7bb2e07a49bd60e187f6d163d5ead81
GaoShang <styleshang@outlook.com>
2025-03-04 16:24:26 -0800

    update file Upload

--- site-patrol-api/file.py
+++ site-patrol-api/file.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
 from fastapi.responses import RedirectResponse
 import aioboto3
 import uuid
 from pydantic_settings import BaseSettings
 from pydantic import BaseModel
 from urllib import parse
+from botocore.config import Config
 
 PATH_PREFIX = "uploads/site_patrol/"
 
@@ -42,6 +43,16 @@ async def generate_upload_url(file_info: FileInfo) -> PreSignedUrl:
         upload_url=url,
         download_url="/api/v1/file?file_path=" + parse.quote(file_path),
     )
+
+
+@file_router.post("/upload")
+async def upload(file: UploadFile):
+    key = f"upload/{uuid.uuid4()}/{file.filename}"
+    # 这里使用 with 没问题，上传完就结束了
+    async with make_s3_client() as client:
+        file_path = f"{uuid.uuid4()}/{file.filename}"
+        await client.upload_fileobj(file.file, settings.s3_bucket, PATH_PREFIX + file_path)
+    return f"/api/v1/file/download?key={parse.quote(key)}"
 
 
 @file_router.post("/download")
